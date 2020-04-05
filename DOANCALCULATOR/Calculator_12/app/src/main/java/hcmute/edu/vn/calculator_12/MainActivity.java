46839df0dcbbe53eada3653a9b8d9ce9c4cf6a29
lampham9 <43400884+lampham9@users.noreply.github.com>
2020-04-05 21:14:28 +0700

    di động

--- DOANCALCULATOR/Calculator_12/app/src/main/java/hcmute/edu/vn/calculator_12/MainActivity.java
+++ DOANCALCULATOR/Calculator_12/app/src/main/java/hcmute/edu/vn/calculator_12/MainActivity.java
@@ -2,22 +2,13 @@ package hcmute.edu.vn.calculator_12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn = (Button) findViewById(R.id.btn_7);
-
-
     }
 }
